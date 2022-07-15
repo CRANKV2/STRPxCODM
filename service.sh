463d007d43663f6be51c06306e29dc9fd40036c3
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2022-07-15 23:55:27 +0200

    New Version 1.1
    ## STRP x CODM 
    ### v1.1 - Reworked some...
    - Converted All Props Into One Script File
    - Added NEW BAG 1.7 PROPS | Its a BEAST Now for FPS Games!
    - Script File Execute them self after every Reboot!

---
+++ service.sh
@@ -0,0 +1,22 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+INFO=/data/adb/modules/.STRPxPUBG-files
+MODID=STRPxPUBG
+LIBDIR=/system
+MODPATH=/data/adb/modules/STRPxPUBG
+wait_until_login() {
+while [[ `getprop sys.boot_completed` -ne 1 && -d "/sdcard" ]]
+do
+sleep 2
+done
+local test_file="/sdcard/.PERMISSION_TEST"
+touch "$test_file"
+while [ ! -f "$test_file" ]; do
+touch "$test_file"
+sleep 2
+done
+rm "$test_file"
+}
+wait_until_login
+sleep 15
+CODMPROPS
